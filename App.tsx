ab13cb3239dbe5d86e57ceb1366bda0a5eb71103
Farvez Hossen <56191538+Farvez999@users.noreply.github.com>
2024-03-09 17:38:38 +0600

    first commit

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -6,113 +7,151 @@
  */
 
 import React from 'react';
-import type {PropsWithChildren} from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+
+
+// eslint-disable-next-line prettier/prettier
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
+  Image,
+  SafeAreaView, StyleSheet, Text, View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+function App() {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <SafeAreaView>
+      <View style={styles.container}>
+        <View style={styles.box}>
+          <View style={styles.profileImage}>
+            <Image
+              style={styles.profileLogo}
+              source={{
+                uri: 'https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1.jpeg',
+              }}
+            />
+          </View>
+          <View style={styles.profile}>
+            <Text style={styles.profileName}>The Octocat</Text>
+            <Text style={styles.profileName}>octocat</Text>
+            <Text style={styles.profileName}>Joind 25 jan 2024</Text>
+          </View>
         </View>
-      </ScrollView>
+        <View>
+          <Text style={styles.discription}>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur cumque reiciendis qui animi voluptatum asperiores iure repudiandae enim consequuntur voluptatem.
+          </Text>
+        </View>
+        <View style={styles.followingCard}>
+          <View style={styles.repos}>
+            <Text style={styles.textH}>Repos</Text>
+            <Text style={styles.textP}>8</Text>
+          </View>
+          <View style={styles.repos}>
+            <Text style={styles.textH}>Followers</Text>
+            <Text style={styles.textP}>3938</Text>
+          </View>
+          <View style={styles.repos}>
+            <Text style={styles.textH}>Following</Text>
+            <Text style={styles.textP}>9</Text>
+          </View>
+        </View>
+        <View>
+          <View style={styles.addressItem}>
+            <Icon name="rocket" size={30} color="#900" />
+            <Text style={styles.addressText}>San Francisco</Text>
+          </View>
+          <View style={styles.addressItem}>
+            <Icon name="rocket" size={30} color="#900" />
+            <Text style={styles.addressText}>https://github.com/Farvez999</Text>
+          </View>
+          <View style={styles.addressItem}>
+            <Icon name="rocket" size={30} color="#900" />
+            <Text style={styles.addressText}>Not Available</Text>
+          </View>
+          <View style={styles.addressItem}>
+            <Icon name="rocket" size={30} color="#900" />
+            <Text style={styles.addressText}>@github</Text>
+          </View>
+        </View>
+      </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    // flex: 1,
+    // backgroundColor: 'white',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  box: {
+    // flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    color: 'black',
+    marginTop: 20,
   },
-  sectionDescription: {
-    marginTop: 8,
+  profileImage: {
+    marginLeft: 20,
+  },
+  profileLogo: {
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+  },
+
+  profile: {
+    padding: 20,
+  },
+
+  profileName: {
+    // marginLeft: 20,
+  },
+
+  discription: {
+    padding: 20,
+    fontWeight: 'bold',
     fontSize: 18,
-    fontWeight: '400',
+    fontFamily: 'Helvetica',
   },
-  highlight: {
-    fontWeight: '700',
+
+  followingCard: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    backgroundColor: '#DDDDDD',
+    margin: 20,
+    borderRadius: 10,
+    padding: 20,
   },
+
+  repos: {
+    alignItems: 'center',
+    fontWeight: 'bold',
+  },
+
+  textH: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    color: 'gray',
+  },
+
+  textP: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    color: 'black',
+    marginTop: 10
+  },
+
+  addressItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginLeft: 20,
+  },
+
+  addressText: {
+    marginLeft: 10,
+    color: 'black',
+  },
+
 });
 
 export default App;
